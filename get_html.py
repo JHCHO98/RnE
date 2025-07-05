3ccaf0cdf7bf0d1904902a3b25a49cd413bff8b6
JHCHO98 <devjoonhee@gmail.com>
2025-07-05 16:04:43 +0900

    Update get_html.py

--- get_html.py
+++ get_html.py
@@ -3,18 +3,14 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 options = Options()
-options.add_argument("user-data-dir=C:/Users/Owner/Desktop")  # 사용자 프로필 경로
+options.add_argument("user-data-dir=C:/Users/Owner/Desktop/selenium_profile")  # 사용자 프로필 경로
 options.add_argument("profile-directory=selenium_profile")  # 정확히 로그인된 프로필 이름
-options.add_argument("--start-maximized")
-options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_argument("--no-first-run")
-options.add_argument("--no-default-browser-check")
 options.add_argument("https://www.youtube.com")  # 실행 직후 이동할 URL
 
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.youtube.com")
 
-time.sleep(100)  # 충분히 로딩 대기
+time.sleep(5)  # 충분히 로딩 대기
 
 html = driver.page_source
 with open("main.html", "w", encoding="utf-8") as file:
